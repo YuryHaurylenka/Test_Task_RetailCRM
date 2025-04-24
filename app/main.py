423eb69c0a8bac4c330cea750efc9bc0d870619a
YuryHaurylenka <gshshaaha@gmail.com>
2025-04-24 18:01:59 +0300

    introduce create_app factory with api_prefix and root redirect endpoint.

--- app/main.py
+++ app/main.py
@@ -4,14 +4,16 @@ from starlette.responses import RedirectResponse
 from app.api import api_router
 from app.core.config import settings
 
-app = FastAPI(
-    title=settings.project_name,
-    debug=settings.debug,
-)
 
-app.include_router(api_router)
+def create_app() -> FastAPI:
+    app = FastAPI(title=settings.project_name, debug=settings.debug)
+    app.include_router(api_router, prefix=settings.api_prefix)
+
+    @app.get("/", include_in_schema=False)
+    async def root() -> RedirectResponse:
+        return RedirectResponse(url="/docs")
+
+    return app
 
 
-@app.get("/", include_in_schema=False)
-async def root():
-    return RedirectResponse(url="/docs")
+app = create_app()
